594111607169de1c939e2a3fbd928005759f857c
Fizal Mohamed <fizalihsan@gmail.com>
2019-11-30 16:42:04 -0500

    Fixing test

--- stock-market/src/app/model/stock.spec.ts
+++ stock-market/src/app/model/stock.spec.ts
@@ -2,6 +2,6 @@ import { Stock } from './stock';
 
 describe('Stock', () => {
   it('should create an instance', () => {
-    expect(new Stock()).toBeTruthy();
+    expect(new Stock('test stock', 'ts', 10, 8)).toBeTruthy();
   });
 });
