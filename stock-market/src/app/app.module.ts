981c05108c6a02597ecc16fe0fbc9efb9982219f
Fizal Mohamed <fizalihsan@gmail.com>
2019-11-30 21:58:25 -0500

    renaming component folder

--- stock-market/src/app/app.module.ts
+++ stock-market/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { StockItemComponent } from './stock/stock-item/stock-item.component';
-import { StockItem2Component } from './stock/stock-item2/stock-item2.component';
+import { StockItemComponent } from './component/stock-item/stock-item.component';
+import { StockItem2Component } from './component/stock-item2/stock-item2.component';
 
 @NgModule({
   declarations: [
